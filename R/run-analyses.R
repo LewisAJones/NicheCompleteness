c8b880d463aa2723a3a1103a157334fe4cd64915
Lewis A. Jones <41071747+LewisAJones@users.noreply.github.com>
2022-03-04 22:23:00 +0100

    Create run-analyses.R

---
+++ R/run-analyses.R
@@ -0,0 +1,14 @@
+## ---------------------------
+##
+## Script name: run-analysis.R
+##
+## Purpose of script: runs analysis
+##
+## Author: Dr Lewis Jones
+##
+## Date Created: 2022-03-04
+##
+## Copyright (c) Lewis Jones, 2022
+## Email: LewisA.Jones@outlook.com
+##
+#---------Analyses-----------
