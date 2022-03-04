8d5bf79f56dac86257b03f84c72505f234126f63
Lewis A. Jones <41071747+LewisAJones@users.noreply.github.com>
2022-03-04 22:13:41 +0100

    initial repo setup
    Initial set up for repo

---
+++ R/options.R
@@ -0,0 +1,16 @@
+## ---------------------------
+##
+## Script name: options.R
+##
+## Purpose of script: provide options for main analyses
+##
+## Author: Dr Lewis Jones
+##
+## Date Created: 2022-03-04
+##
+## Copyright (c) Lewis Jones, 2022
+## Email: LewisA.Jones@outlook.com
+##
+#-------Analyses options---------
+res = 1
+n_species = 1000
